45bce995986d5c2507214d1c2c5410cf12c81df2
Youenn <156934138+Monsteryou@users.noreply.github.com>
2024-09-24 16:56:39 +0200

    RSA
    Test RSA avec Matteo Lepelletier

--- cryptoCielTest.cpp
+++ cryptoCielTest.cpp
@@ -6,24 +6,45 @@
 
 int main()
 {
+
+    /*
+            * SHA256 - (4) Question 2,3,4,5
+
     HashGestion LM;
-    std::string File = "test.txt";
-    std::cout << "SHA256 Hash: " << LM.CalculateSHA256(File) << std::endl;
-    return(0);
-    
-    AesGestion aes;
-    aes.GenerateAESKey();
-    aes.SaveAESKeyToFile("cle_aes.txt");
-    aes.LoadAESKeyFromFile("cle_aes.txt");
-    aes.EncryptFileAES256("cle_aes.txt" , "cle_aes_crypt.txt");
-    aes.DecryptFileAES256("cle_aes_crypt.txt","cle_aes_decrypt.txt");
+    std::string monMessageHash = "Crypto.txt";
+    std::cout << LM.CalculateSHA256(monMessageHash) << std::endl;
+
+
+
+            * AES - (5) Question 6,7,8,9
+
+    AesGestion AES;
+    AES.GenerateAESKey();
+    AES.SaveAESKeyToFile("ClefAES.txt");
+    AES.EncryptFileAES256("cle_aes.txt", "cle_aes_crypt.txt");
+    AES.DecryptFileAES256("cle_aes_crypt.txt","cle_aes_decrypt.txt");
+
+            * RSA - (6.1) 1er Programme
+    */
 
     RsaGestion RSA;
-    RSA.generationClef("RSAPublic.pem", "RSAPrive.pem", 4096);
-    std::string MessageEncrypt = "Chien";
+    RSA.generationClef("YouennRSAPublic.pem", "YouennRSAPrive.pem", 2048);
+
+    /*
+    std::string MessageEncrypt = "Chien ";
     std::string MessageCrypt = RSA.chiffrementRsa(MessageEncrypt);
     std::cout << MessageCrypt << std::endl;
     std::string MessageDecrypt = RSA.dechiffrementRsa(MessageCrypt);
     std::cout << MessageDecrypt << std::endl;
 
+            * RSA - (6.2) 2eme Programmes
+    RSA.chargementClefs("YouennRSAPublic.pem", "YouennRSAPrive.pem")
+    RSA.chiffreDansFichier("YouennRSADonnee.txt", "YouennRSAChiffre.txt");
+    RSA.dechiffrementFichier("YouennRSADonnee.txt", "YouennRSADechiffre.txt", 2048);
+    */
+   
+    RSA.chargementClefs("YouennRSAPublic.pem", "YouennRSAPrive.pem");
+    RSA.chiffrementFichier("YouennRSADonnee.txt", "YouennRSAChiffre.txt", 2048); 
+    RSA.dechiffrementFichier("YouennRSAChiffre.txt", "YouennRSADechiffre.txt", 2048);
+    
 }
